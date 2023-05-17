2e3dd2996a77fa47f19775e4cafef216a3d624f0
Gianni GIUDICE <gg@vitalytech.com>
2023-05-17 15:43:51 +0200

    :recycle:

---
+++ src/Service/HttpClientWrapper.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace VTGianni\OopsBundle\Service;
+
+
+use Exception;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class HttpClientWrapper
+{
+    private HttpClientInterface $client;
+    private OopsService $service;
+
+    public function __construct(HttpClientInterface $client, OopsService $service)
+    {
+        $this->client = $client;
+        $this->service = $service;
+    }
+
+    /**
+     * @param string $method
+     * @param string $url
+     * @param array $options
+     * @return ResponseInterface
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws Exception
+     */
+    public function request(string $method, string $url, array $options = []): ResponseInterface
+    {
+        $response = $this->client->request($method, $url, $options);
+
+        // Here you can do something with the response, for example:
+        if ($response->getStatusCode() >= 400) {
+            // Report the error.
+            $this->service->reportError(
+                $url,
+                $response->getStatusCode(),
+                "Error {$response->getStatusCode()}",
+                $options['headers'] ?? [],
+                $options['body'] ?? [],
+                $response->toArray()
+            );
+        }
+
+        return $response;
+    }
+}
