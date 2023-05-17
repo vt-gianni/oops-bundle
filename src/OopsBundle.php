b6d175393c5a83ba04671942f65a70e0856d5e1f
Gianni GIUDICE <gg@vitalytech.com>
2023-05-17 14:37:59 +0200

    :wheelchair: services.yaml data automatically downloaded

--- src/OopsBundle.php
+++ src/OopsBundle.php
@@ -5,8 +5,16 @@ namespace VTGianni\OopsBundle;
 
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use VTGianni\OopsBundle\DependencyInjection\OopsExtension;
 
 class OopsBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new OopsExtension();
+        }
 
+        return $this->extension;
+    }
 }
